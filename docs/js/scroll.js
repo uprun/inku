109e43bc9ff7c817cceb2e60b13cb28d9e2b8ee6
Oleksandr Kryvonos <geraclion@gmail.com>
2022-07-30 17:06:34 +0300

    new line drawing

--- docs/js/scroll.js
+++ docs/js/scroll.js
@@ -2,58 +2,22 @@ if(typeof(lookup) === 'undefined')
 {
     lookup = {};
 }
-lookup.globalOffsetX = ko.observable(0.0);
-lookup.globalOffsetY = ko.observable(0.0);
-lookup.globalOffsetZ = ko.observable(1.0);
-lookup.globalMaxX = ko.observable(2048);
-lookup.globalMaxY = ko.observable(2048);
-lookup.globalMinX = ko.observable(-2048);
-lookup.globalMinY = ko.observable(-2048);
 
-lookup.pointerType = ko.observable("");
+lookup.mouseIsDown = false;
 
-lookup.bodyOnWheel = function() {
-    
-    //console.log(event);
-    //lookup.applyMovement(deltaY, deltaX);
-  
-    //scale += event.deltaY * -0.01;
-};
-
-lookup.bodyOnPointerMove = function()
-{
-    //console.log(event);
-    if(event.shiftKey)
-    {
-        lookup.createPoint({x: event.pageX, y: event.pageY});
-    }
-
-    if(event.buttons > 0)
-    {
-        lookup.createPoint({x: event.pageX, y: event.pageY});
-    }
-
-    lookup.pointerType(event.pointerType);
-
-};
-
-lookup.previosTouch = undefined;
-
-lookup.bodyOnTouchMove = function()
-{
-    //console.log(event);
-    var touches = event.changedTouches;
-    if(touches.length > 0 )
-    {
-        const clientX = touches[0].clientX;
-        const clientY = touches[0].clientY;
-        lookup.createPoint({x: clientX, y: clientY});
-    }
-
-    lookup.pointerType(event.pointerType);
-};
-
-lookup.bodyOnTouchEnd = function()
-{
-    lookup.previosTouch = undefined;
-};
+window.addEventListener("pointerdown", function(e) {
+    lookup.createPoint({x: e.pageX, y: e.pageY, start: true, finish: false});
+    lookup.mouseIsDown = true;
+  });
+window.addEventListener("pointerup", function(e) {
+    lookup.mouseIsDown = false;
+    lookup.createPoint({x: e.pageX, y: e.pageY, start: false, finish: true});
+  });
+window.addEventListener("pointermove", function(event) {
+   if(lookup.mouseIsDown) {
+      let events = event.getCoalescedEvents();
+      for(let e of events) {
+        lookup.createPoint({x: e.pageX, y: e.pageY, start: false, finish: false});
+      }
+   }
+  });
