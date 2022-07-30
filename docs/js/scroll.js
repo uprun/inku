c987565cf75975d3175b977fa28b726bf2d6a9fa
Oleksandr Kryvonos <geraclion@gmail.com>
2022-07-30 18:03:00 +0300

    adding back touchmove event handlers

--- docs/js/scroll.js
+++ docs/js/scroll.js
@@ -13,18 +13,51 @@ window.addEventListener("pointerup", function(e) {
     lookup.mouseIsDown = false;
     lookup.createPoint({x: e.pageX, y: e.pageY, start: false, finish: true});
   });
-window.addEventListener("pointermove", function(event) {
+
+lookup.bodyOnPointerMove = function(event) 
+{
    if(lookup.mouseIsDown) {
       let events = event.getCoalescedEvents();
       for(let e of events) {
         lookup.createPoint({x: e.pageX, y: e.pageY, start: false, finish: false});
       }
    }
-  });
+};
+
+lookup.previosTouch = undefined;
 
 lookup.bodyOnTouchEnd = function(e)
 {
     console.log("bodyOnTouchEnd");
     lookup.mouseIsDown = false;
-    lookup.createPoint({x: e.pageX, y: e.pageY, start: false, finish: true});
+    lookup.previosTouch = undefined;
+    if(event.changedTouches.length > 0)
+    {
+      var touch = event.changedTouches[0];
+      lookup.createPoint({x: touch.pageX, y: touch.pageY, start: false, finish: true});
+    }
+};
+
+
+
+lookup.bodyOnTouchMove = function()
+{
+    //console.log(event);
+    var touches = event.changedTouches;
+    if(touches.length > 0 )
+    {
+        const clientX = touches[0].clientX;
+        const clientY = touches[0].clientY;
+        if(typeof(lookup.previosTouch) === "undefined")
+        {
+            lookup.createPoint({x: clientX, y: clientY, start: true, finish: false});
+            lookup.previosTouch = true;
+        }
+        else
+        {
+            lookup.createPoint({x: clientX, y: clientY, start: false, finish: false});
+        }
+    }
+
+    lookup.pointerType(event.pointerType);
 };
